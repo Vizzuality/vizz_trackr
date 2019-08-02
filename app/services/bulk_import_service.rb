5f2947e827a6313d3fa3b662b86c38369aa96a19
Simao Belchior <simaobelchior@gmail.com>
2019-08-02 10:52:22 +0100

    adjust to small issues

--- app/services/bulk_import_service.rb
+++ app/services/bulk_import_service.rb
@@ -16,6 +16,7 @@ class BulkImportService
 
   def import_data
     CSV.read(@file, headers: true).each do |row|
+      next unless row.first[1]
       name = row.first[1].split(',').map(&:strip)
       user = User.where(name: [name[1], name[0]].join(" ")).first
       if !user
@@ -35,8 +36,10 @@ class BulkImportService
         else
           puts "Found project #{project.name} with this value: #{pair[0]}"
         end
+        val = pair[1].gsub("%", "").to_f
+        val = val < 1.0 && val > 0.0 ? val *100 : val
         report.report_parts.new(project_id: project.id,
-                                percentage: pair[1].gsub("%", "").to_f)
+                                percentage: val)
       end
       report.save!
     end
