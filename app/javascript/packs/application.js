d4694ac1131124e5a2665764cf174cead6b9b166
Simao Belchior <simaobelchior@gmail.com>
2019-08-03 09:38:29 +0100

    Adds some chart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("chartkick")
+require("chart.js")
 
 import 'bootstrap'
 import '../stylesheets/application'
