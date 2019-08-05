c8993bb94f9b0bd1ddfb0a06c02a0ec8b593404c
Simao Belchior <simaobelchior@gmail.com>
2019-08-05 09:12:59 +0100

    rename column

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.order(:name)
+    @projects = Project.order(:name).includes(:report_parts)
   end
 
   # GET /projects/1
