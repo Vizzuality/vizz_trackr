a2732cd91bec8eb085c32696438acb037d77759f
Simao Belchior <simaobelchior@gmail.com>
2019-08-07 16:45:26 +0100

    adds more includes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.order(:name).includes(:report_parts)
+    @projects = Project.order(:name).includes(:report_parts, :contracts)
   end
 
   # GET /projects/1
