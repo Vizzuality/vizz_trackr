38a95c0a1371bf2eb350b7401518e485d7cb5663
Simao Belchior <simaobelchior@gmail.com>
2019-08-10 10:47:50 +0100

    code style change

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,8 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.order(:name).includes(:report_parts, :contracts)
+    @projects = Project.order(:name).
+      includes(:report_parts, :contracts)
   end
 
   # GET /projects/1
