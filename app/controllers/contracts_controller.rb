3838001729e57939b6ee9dd2c8d7f49602461cb3
Simao Belchior <simaobelchior@gmail.com>
2019-11-16 20:11:17 +0000

    rubocop changes for contracts_controller

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,54 +1,55 @@
 class ContractsController < ApplicationController
   before_action :set_contract, only: [:show, :reports]
   def index
-    @contracts = Contract.joins(:project).includes(:full_reports, :project).
-      order("projects.name ASC, contracts.name ASC")
+    @contracts = Contract.joins(:project).includes(:full_reports, :project)
+      .order('projects.name ASC, contracts.name ASC')
   end
 
   def show
     @contract = Contract.find(params[:id])
     agg = 0.0
-    contract = { name: @contract.name, data: {}}
-    aggregate = { name: "Aggregate", data: {}}
-    @contract.full_reports.
-      select("reporting_period_name, sum(cost) AS cost").
-      group(:reporting_period_name).
-      order("TO_DATE(reporting_period_name, 'MonthYYYY') ASC").each do |report|
+    contract = {name: @contract.name, data: {}}
+    aggregate = {name: 'Aggregate', data: {}}
+    @contract.full_reports
+      .select('reporting_period_name, sum(cost) AS cost')
+      .group(:reporting_period_name)
+      .order("TO_DATE(reporting_period_name, 'MonthYYYY') ASC").each do |report|
         contract[:data][report.reporting_period_name] = report.cost
         agg += report.cost
         aggregate[:data][report.reporting_period_name] = agg
       end
     budget = {
       name: 'Budget',
-      data: contract[:data].map{|k,_| [k, @contract.budget.try(:to_f)]}.to_h,
+      data: contract[:data].map { |k, _| [k, @contract.budget.try(:to_f)] }.to_h,
       points: false
     }
     @data = [contract, aggregate, budget]
   end
 
+  # rubocop:disable Metrics/AbcSize
   def reports
-    @reporting_periods = ReportingPeriod.joins(:full_reports).
-      where(full_reports: { contract_id: @contract.id }).
-      order(:date).distinct
+    @reporting_periods = ReportingPeriod.joins(:full_reports)
+      .where(full_reports: {contract_id: @contract.id})
+      .order(:date).distinct
     @data = []
     @reporting_periods.each do |rp|
       next if params[:reporting_period_id] &&
         rp.id != params[:reporting_period_id].to_i
 
       rp.full_reports.for_contract(@contract.id).each do |report|
-        entry = @data.select{|t| t[:name] == report.user_name }.first
-        if !entry
-          entry = { name: report.user_name }
+        entry = @data.select { |t| t[:name] == report.user_name }.first
+        unless entry
+          entry = {name: report.user_name}
           new_entry = true
         end
         entry[:data] = {} unless entry[:data]
         entry[:data][report.reporting_period_name] = report.days
         @data << entry if new_entry
-        new_entry = false
       end
     end
-    @data.sort!{|a,b| b[:data].size <=> a[:data].size}
+    @data.sort! { |a, b| b[:data].size <=> a[:data].size }
   end
+  # rubocop:enable Metrics/AbcSize
 
   private
 
