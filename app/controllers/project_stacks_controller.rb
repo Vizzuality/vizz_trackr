dbfd70e63393ed8f8fb47d0ea8ab33fd85c6ad37
Simao Belchior <simaobelchior@gmail.com>
2019-11-17 23:10:47 +0000

    Rubocop changes to project_stacks

--- app/controllers/project_stacks_controller.rb
+++ app/controllers/project_stacks_controller.rb
@@ -1,4 +1,5 @@
 class ProjectStacksController < ApplicationController
+  # rubocop:disable Metrics/AbcSize
   def index
     @roles = Role.order(:name)
     @reporting_periods = ReportingPeriod.order(:date)
@@ -8,18 +9,18 @@ class ProjectStacksController < ApplicationController
     projects = Project.where(id: ReportPart.select(:project_id).distinct.map(&:project_id))
 
     projects.each do |p|
-      ["K", "Rosling"].each do |team|
+      %w[K Rosling].each do |team|
         t = Team.where(name: team).first
-        entry = { name: p.name, stack: t.name }
+        entry = {name: p.name, stack: t.name}
         entry[:data] = {}
-        ReportPart.where(project_id: p.id).
-          joins(report: :reporting_period).
-          where(reports: { team_id: t.id }).
-          order('reporting_periods.date ASC').each do |rp|
+        ReportPart.where(project_id: p.id)
+          .joins(report: :reporting_period)
+          .where(reports: {team_id: t.id})
+          .order('reporting_periods.date ASC').each do |rp|
             the_name = rp.report.reporting_period.display_name
             entry[:data][the_name] = (entry[:data][the_name] || 0) + rp.days
           end
-        @data << entry if !entry[:data].empty?
+        @data << entry if entry[:data].present?
       end
     end
 
@@ -29,16 +30,17 @@ class ProjectStacksController < ApplicationController
       format.js
     end
   end
+  # rubocop:enable Metrics/AbcSize
 
   private
 
   def cache_key
-    last_update = Report.maximum(:updated_at).strftime("%d%m%Y-%H%M")
+    last_update = Report.maximum(:updated_at).strftime('%d%m%Y-%H%M')
     [
-      "project-stacks",
-      "role-#{params[:role_id].presence || "all"}",
-      "threshold-#{params[:threshold].presence || "all"}",
+      'project-stacks',
+      "role-#{params[:role_id].presence || 'all'}",
+      "threshold-#{params[:threshold].presence || 'all'}",
       last_update
-    ].join("-")
+    ].join('-')
   end
 end
