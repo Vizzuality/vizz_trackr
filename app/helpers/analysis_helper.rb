d6e106a90b6aca424b91e90213477e8465ba90d0
Simao Belchior <simaobelchior@gmail.com>
2019-11-17 22:53:18 +0000

    analysis helper rubocoped

--- app/helpers/analysis_helper.rb
+++ app/helpers/analysis_helper.rb
@@ -1,10 +1,9 @@
 module AnalysisHelper
-
   def line_chart_data data, filter
     filter = filter.try(:to_sym) || :mean
     chart_data = []
     [:K, :Rosling].each do |team|
-      hsh = { name: team.to_s }
+      hsh = {name: team.to_s}
       actual_data = {}
       data.each do |dt|
         actual_data[dt[:rp].display_name] = dt[team][filter]
