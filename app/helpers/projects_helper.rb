18b8cba7e70a87b3ad0ca2bb007618ec55d8cf2d
Simao Belchior <simaobelchior@gmail.com>
2019-11-17 22:45:10 +0000

    rubocop helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,13 @@
 module ProjectsHelper
   def td_for_burn budget, spend
-    burn = budget && budget > 0.0 ? (spend/budget*100).round(2) : nil
-    klass = burn ?
-      (burn <= 100 ? "success" : "danger") :
-      ""
+    burn = budget && budget > 0.0 ? (spend / budget * 100).round(2) : nil
+    klass = if burn
+              (burn <= 100 ? 'success' : 'danger')
+            else
+              ''
+            end
     content_tag :td, class: "table-#{klass}" do
-      burn ? "#{burn}%" : "-"
+      burn ? "#{burn}%" : '-'
     end
   end
 end
