a735e67721d10466546f2323ddba4c6b8929f0fa
Simao Belchior <simaobelchior@gmail.com>
2019-11-17 23:25:02 +0000

    roles controller rubocop

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -9,8 +9,7 @@ class RolesController < ApplicationController
 
   # GET /roles/1
   # GET /roles/1.json
-  def show
-  end
+  def show; end
 
   # GET /roles/new
   def new
@@ -18,8 +17,7 @@ class RolesController < ApplicationController
   end
 
   # GET /roles/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /roles
   # POST /roles.json
@@ -62,13 +60,14 @@ class RolesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_role
-      @role = Role.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def role_params
-      params.require(:role).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_role
+    @role = Role.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def role_params
+    params.require(:role).permit(:name)
+  end
 end
