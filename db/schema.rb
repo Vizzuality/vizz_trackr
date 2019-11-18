05faac621c43fcb8a2b15fb73e94a6a25c31a867
Simao Belchior <simaobelchior@gmail.com>
2019-11-18 18:37:10 +0000

    Include is_billable in the full_reports view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_174913) do
+ActiveRecord::Schema.define(version: 2019_11_18_183328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_174913) do
   create_view "full_reports", sql_definition: <<-SQL
       SELECT projects.id AS project_id,
       projects.name AS project_name,
+      projects.is_billable AS project_is_billable,
       contracts.id AS contract_id,
       contracts.name AS contract_name,
       reporting_periods.id AS reporting_period_id,
