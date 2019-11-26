7b4c9a286c6bb5ebcdccf8fc7e4b61e392e5477e
Simao Belchior <simaobelchior@gmail.com>
2019-11-26 11:07:54 +0000

    Connect non staff costs with reporting period

--- app/models/non_staff_cost.rb
+++ app/models/non_staff_cost.rb
@@ -2,4 +2,5 @@ class NonStaffCost < ApplicationRecord
   TYPES = %w[outsource travel servers].freeze
   enum type: array_to_enum_hash(TYPES)
   belongs_to :contract
+  belongs_to :reporting_period
 end
