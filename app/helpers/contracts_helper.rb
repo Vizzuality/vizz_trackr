b9fbb7b02900b6415c153d4e32391041ecd43540
Miguel Mendoza <info@miguelmendoza.dj>
2019-11-27 12:32:35 +0100

    adds colors to event buttons

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -1,2 +1,14 @@
 module ContractsHelper
+  def event_color(state)
+    case state
+      when 'restart'
+        'btn-primary'
+      when 'finish'
+        'btn-warning'
+      when 'start'
+        'btn-success'
+      else
+        'btn-success'
+    end
+  end
 end
