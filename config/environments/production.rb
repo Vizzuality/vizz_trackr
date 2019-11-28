95dbf7f788b61e6a4b4ebac09eaee23da7a48306
Simao Belchior <simaobelchior@gmail.com>
2019-11-28 14:32:36 +0000

    configure host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,6 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: 'vizz-trackr.herokuapp.com', port: 3000 }
+
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
