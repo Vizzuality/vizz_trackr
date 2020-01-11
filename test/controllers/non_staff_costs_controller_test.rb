532f60d8112e009b49af0b9d0af6fc8aec29e9b6
Simao Belchior <simaobelchior@gmail.com>
2020-01-11 17:27:28 +0000

    Fixes non staff costs controller tests

--- test/controllers/non_staff_costs_controller_test.rb
+++ test/controllers/non_staff_costs_controller_test.rb
@@ -1,20 +1,22 @@
 require 'test_helper'
 
 class NonStaffCostsControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::ControllerHelpers
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @request.env['devise.mapping'] = Devise.mappings[:admin]
     sign_in users(:admin)
     @non_staff_cost = non_staff_costs(:one)
   end
 
   test 'should get index' do
     get non_staff_costs_url
+    @request.env['devise.mapping'] = Devise.mappings[:user]
     assert_response :success
   end
 
   test 'should get new' do
     get new_non_staff_cost_url
+    @request.env['devise.mapping'] = Devise.mappings[:user]
     assert_response :success
   end
 
@@ -24,21 +26,25 @@ class NonStaffCostsControllerTest < ActionDispatch::IntegrationTest
         non_staff_cost: {
           contract_id: @non_staff_cost.contract_id,
           cost: @non_staff_cost.cost,
-          cost_type: @non_staff_cost.cost_type
+          cost_type: @non_staff_cost.cost_type,
+          reporting_period_id: reporting_periods(:one).id
         }
       }
+      @request.env['devise.mapping'] = Devise.mappings[:user]
     end
 
-    assert_redirected_to non_staff_cost_url(NonStaffCost.last)
+    assert_redirected_to non_staff_costs_url
   end
 
   test 'should show non_staff_cost' do
     get non_staff_cost_url(@non_staff_cost)
+    @request.env['devise.mapping'] = Devise.mappings[:user]
     assert_response :success
   end
 
   test 'should get edit' do
     get edit_non_staff_cost_url(@non_staff_cost)
+    @request.env['devise.mapping'] = Devise.mappings[:user]
     assert_response :success
   end
 
@@ -50,12 +56,14 @@ class NonStaffCostsControllerTest < ActionDispatch::IntegrationTest
         cost_type: @non_staff_cost.cost_type
       }
     }
+    @request.env['devise.mapping'] = Devise.mappings[:user]
     assert_redirected_to non_staff_cost_url(@non_staff_cost)
   end
 
   test 'should destroy non_staff_cost' do
     assert_difference('NonStaffCost.count', -1) do
       delete non_staff_cost_url(@non_staff_cost)
+      @request.env['devise.mapping'] = Devise.mappings[:user]
     end
 
     assert_redirected_to non_staff_costs_url
