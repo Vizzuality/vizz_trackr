a0115f319d171876c95af9afe0bf3a888fab7bd6
Simao Belchior <simaobelchior@gmail.com>
2020-01-19 19:00:07 +0000

    fix active state of menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -19,15 +26,8 @@ import "controllers";
 $.fn.datepicker.defaults.format = "dd/mm/yyyy";
 
 // Sidebar Menu
-$(document).ready(() => {
+$( document ).on('turbolinks:load', function() {
   setTimeout(function () {
     $(".vertical-nav-menu").metisMenu();
   }, 100);
 });
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
