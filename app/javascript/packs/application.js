e4679d62641d54778e0ffe894b2780b3df9a5b9f
Simao Belchior <simaobelchior@gmail.com>
2020-01-20 07:55:42 +0000

    Enable closing sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,4 +30,20 @@ $( document ).on('turbolinks:load', function() {
   setTimeout(function () {
     $('.vertical-nav-menu').metisMenu();
   }, 100);
+
+
+  $('.close-sidebar-btn').click(function () {
+
+    var classToSwitch = $(this).attr('data-class');
+    var containerElement = '.app-container';
+    $(containerElement).toggleClass(classToSwitch);
+
+    var closeBtn = $(this);
+
+    if (closeBtn.hasClass('is-active')) {
+      closeBtn.removeClass('is-active');
+    } else {
+      closeBtn.addClass('is-active');
+    }
+  });
 });
