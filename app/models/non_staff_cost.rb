5fa6847636fcf42e3cd81427e1b2e07b828e8357
Simao Belchior <simaobelchior@gmail.com>
2020-04-06 16:10:01 +0100

    one more non staff cost type

--- app/models/non_staff_cost.rb
+++ app/models/non_staff_cost.rb
@@ -13,7 +13,7 @@
 #
 
 class NonStaffCost < ApplicationRecord
-  TYPES = %w[outsource travel servers].freeze
+  TYPES = %w[outsource travel servers others].freeze
   enum type: array_to_enum_hash(TYPES)
   belongs_to :contract
   belongs_to :reporting_period
