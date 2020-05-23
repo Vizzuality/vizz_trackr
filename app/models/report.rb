98576fe5da48db0997579b43276685406e0c4395
Simao Belchior <simaobelchior@gmail.com>
2020-05-24 00:22:01 +0100

    Moves role_id from report to report parts
    In order to enable someone reporting as different role/functional
    area we moved the role_id to the report_parts model.
    This commit also remove role_id from reports and updates
    as needed.

--- app/models/report.rb
+++ app/models/report.rb
@@ -15,7 +15,6 @@
 class Report < ApplicationRecord
   belongs_to :user
   belongs_to :team, optional: true
-  belongs_to :role, optional: true
   belongs_to :reporting_period
   has_many :report_parts, dependent: :destroy
   accepts_nested_attributes_for :report_parts, allow_destroy: true
