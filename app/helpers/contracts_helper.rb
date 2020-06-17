9097448e97f066da6a869c7b48069bb49255520b
Simao Belchior <simaobelchior@gmail.com>
2020-06-17 16:33:39 +0100

    Updates to the contracts index table

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -15,26 +15,47 @@ module ContractsHelper
   def result_with_color(result)
     return '?' unless result
 
-    class_name = if result <= 100.0
+    klass = if result <= 100.0
                    'text-primary'
                  else
                    'text-danger'
                  end
-    content_tag(:span, result, class: class_name)
+    content_tag(:span, result, class: klass)
   end
 
-  def td_for_burn burn, progress = 0
-    return content_tag(:td) unless burn
+  def colored_burn burn, income
+    klass = if income && burn > income
+              'text-danger'
+            elsif burn && burn > 0.0
+              'text-success'
+            end
 
-    klass = if burn <= progress
-              'success'
+    content_tag(:span, number_to_currency(burn), class: klass)
+  end
+
+  def progress_bar progress, burn = nil
+    klass = if burn.nil?
+              'primary'
             elsif burn > progress
               'danger'
             else
-              ''
+              'success'
             end
-    content_tag :td, class: "text-#{klass}" do
-      burn ? "#{burn}%" : '-'
+    val = burn ? burn : progress
+    content_tag :div, class: 'widget-content p-0' do
+      content_tag :div, class: 'widget-content-outer' do
+        content_tag :div, class: 'widget-content-wrapper' do
+          content_tag :div, class: 'widget-content-left pr-2 text-right', style: 'width: 80px;' do
+            content_tag(:div, "#{val&.round(2)}%", class: "text-#{klass}")
+          end.concat(
+          content_tag(:div, class: 'widget-content-right w-100') do
+            content_tag(:div, class: 'progress-bar-xs progress') do
+              content_tag(:div, nil, class: "progress-bar bg-#{klass}", role: 'progressbar', 'aria-valuenow': val,
+                'aria-valuemin': 0, 'aria-valuemax': 100, style: "width: #{val}%")
+            end
+          end)
+        end
+      end
     end
   end
 end
