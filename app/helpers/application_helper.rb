a65a574cf7ca69600a999c581b1c58d5479ee75c
Simao Belchior <simaobelchior@gmail.com>
2020-06-17 16:57:09 +0100

    abstract progress bar for table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
       text
     end
   end
+
+  def progress_bar_for_table val, klass
+    content_tag :div, class: 'widget-content p-0' do
+      content_tag :div, class: 'widget-content-outer' do
+        content_tag :div, class: 'widget-content-wrapper' do
+          content_tag :div, class: 'widget-content-left pr-2 text-right', style: 'width: 80px;' do
+            content_tag(:div, "#{val&.round(2)}%", class: "text-#{klass}")
+          end.concat(
+          content_tag(:div, class: 'widget-content-right w-100') do
+            content_tag(:div, class: 'progress-bar-xs progress') do
+              content_tag(:div, nil, class: "progress-bar bg-#{klass}", role: 'progressbar', 'aria-valuenow': val,
+                'aria-valuemin': 0, 'aria-valuemax': 100, style: "width: #{val}%")
+            end
+          end)
+        end
+      end
+    end
+  end
 end
--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -42,20 +42,6 @@ module ContractsHelper
               'success'
             end
     val = burn ? burn : progress
-    content_tag :div, class: 'widget-content p-0' do
-      content_tag :div, class: 'widget-content-outer' do
-        content_tag :div, class: 'widget-content-wrapper' do
-          content_tag :div, class: 'widget-content-left pr-2 text-right', style: 'width: 80px;' do
-            content_tag(:div, "#{val&.round(2)}%", class: "text-#{klass}")
-          end.concat(
-          content_tag(:div, class: 'widget-content-right w-100') do
-            content_tag(:div, class: 'progress-bar-xs progress') do
-              content_tag(:div, nil, class: "progress-bar bg-#{klass}", role: 'progressbar', 'aria-valuenow': val,
-                'aria-valuemin': 0, 'aria-valuemax': 100, style: "width: #{val}%")
-            end
-          end)
-        end
-      end
-    end
+    progress_bar_for_table val, klass
   end
 end
